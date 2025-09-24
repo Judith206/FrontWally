93789d36df8f829ce69377d8f65d814e3eeb4a6a
cp25001@esfe.agape.edu.sv <cp25001@esfe.agape.edu.sv>
2025-09-23 22:37:21 -0600

    _Layout.cshtml  botón para cerrar la sesión, creacion de carpeta ViewComponents y clase UserInfoViewComponent,  carpeta Components y dentro de Components una carpeta llamada UserInfo y en _Layout.cshtml finalizacion de login

---
+++ FrontWally/Views/ViewComponents/UserInfoViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FrontWally.Views.ViewComponents
+{
+    public class UserInfoViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var user = HttpContext.User;
+            if(user.Identity != null && user.Identity.IsAuthenticated)
+            {
+                var nombre = user.FindFirstValue(ClaimTypes.Name);
+                var rol = user.FindFirstValue(ClaimTypes.Role);
+
+                ViewBag.Nombre = nombre;
+                ViewBag.Rol = rol;
+            }
+            else
+            {
+                ViewBag.Nombre = "Invitado";
+                ViewBag.Rol = "";
+            }
+            return View();
+        }
+    }
+}
